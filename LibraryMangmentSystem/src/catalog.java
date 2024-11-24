ec697a5e104c4d74e6ab4736b96947781e4ad1bc
AL-Shwadfy <doctorezz24@gmail.com>
2024-11-24 22:59:34 +0200

    uss

--- LibraryMangmentSystem/src/catalog.java
+++ LibraryMangmentSystem/src/catalog.java
@@ -1,7 +1,17 @@
 public class catalog {
     linkedlist linkedlist = new linkedlist();
- public  Boolean add(){
 
+    public Boolean add(Book book) {
+        if (book == null) {
+            System.out.println("error");
+            return false;
+        } else if (linkedlist.search(book)) {
+            System.out.println("Book already exists");
+            return false;
+        } else {
+            linkedlist.insertAtBeginning(book);
+            return true;
+        }
+     }
 
- }
 }
