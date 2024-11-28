aef222cc9cb2c2b628fa286f80b703706e202575
mohamedahmedattiaa <mohamedahmedattia7@gmail.com>
2024-11-28 03:07:52 +0200

    improve borrow and return book method

--- LibraryMangmentSystem/src/Member.java
+++ LibraryMangmentSystem/src/Member.java
@@ -2,50 +2,31 @@ public class Member {
     private String bookId;
     private String name;
     private String contactInfo;
-    private String booksBorrowed;
-    private linkedlist linkedlist;
+    private linkedlist booksBorrowed;
     private IDGenerator idGenerator;
 
-    public Member( String name, String contactInfo, String booksBorrowed) {
+    public Member( String name, String contactInfo) {
         this.bookId = IDGenerator.generateMemberID();
         this.name = name;
         this.contactInfo = contactInfo;
-        this.booksBorrowed = booksBorrowed;
+        this.booksBorrowed = new linkedlist();
+
     }
 
-    public String getBookId() {
-        return bookId;
-    }
-
-
+    public String getBookId() {return bookId;}
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getContactInfo() {
         return contactInfo;
     }
 
-    public void setContactInfo(String contactInfo) {
-        this.contactInfo = contactInfo;
-    }
-
-    public String getBooksBorrowed() {
-        return booksBorrowed;
-    }
-
-    public void setBooksBorrowed(String booksBorrowed) {
-        this.booksBorrowed = booksBorrowed;
-    }
 
     public void borrowBook(Book book) {
         if(book.getAvailablityStatus()) {
-            linkedlist.insertAtBeginning(book);
+            booksBorrowed.insertAtBeginning(book);
           book.setAvailablityStatus(false);
             System.out.println(name+" Book Borrowed successfully "+book.getBookTitle());
         }else{
@@ -54,7 +35,8 @@ public class Member {
     }
 
     public void returnBook(Book book) {
-        if(!linkedlist.search(book.getBookID())){
+        if(booksBorrowed.Search(book.getBookID())){
+            booksBorrowed.delete(book.getBookID());
             book.setAvailablityStatus(true);
             System.out.println(name+" Book Returned successfully "+book.getBookTitle());
         }else{
