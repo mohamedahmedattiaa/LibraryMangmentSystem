2201c6b3fbc821fa0d7fd4f72366d7da1d7e18d7
AL-Shwadfy <doctorezz24@gmail.com>
2024-11-28 21:55:58 +0200

    uss

--- LibraryMangmentSystem/src/Loan.java
+++ LibraryMangmentSystem/src/Loan.java
@@ -1,7 +1,7 @@
 
 import java.time.LocalDate;
 import java.util.Date;
-public class Loan  {
+public class Loan extends PendingRequestsQueue  {
     private String loanID;
     private String bookId;
     private String memberId;
@@ -47,17 +47,21 @@ public class Loan  {
     public boolean isReturned() {
         return returnDate != null;
     }
-
+    PendingRequestsQueue P = new PendingRequestsQueue();
     public void borrowBook( Member member ,String bookid) {  // should we send catalog not a book , and member ID ??
         Book book = catalog.searchBook(bookid);
         if (book != null && book.getAvailablityStatus()) {
             book.setAvailablityStatus(false);
             //serach for member
             System.out.println(member.getName() + " borrowed " + book.getBookTitle() + " on " + issueDate);
-         new Loan();
+
          bookId = bookid;
          memberId = member.getmemberId();
             System.out.println("Book is borrowed.");
+        }
+        if (book != null && book.getAvailablityStatus()==false){
+
+            P.enqueue(bookid,member.getmemberId());
         }
         else {
             System.out.println("Book is not available.");
@@ -70,6 +74,9 @@ public class Loan  {
             book.setAvailablityStatus(true);
             this.returnDate = new Date();
             System.out.println(member.getName() + " returned " + book.getBookTitle() + " on " + returnDate);
+            if(P.equals(book)){
+                P.dequeue();
+            }
         } else {
             System.out.println("This book is not part of the catalog.");
         }
--- LibraryMangmentSystem/src/PendingRequestsQueue.java
+++ LibraryMangmentSystem/src/PendingRequestsQueue.java
@@ -1,17 +1,18 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class PendingRequestsQueue {
-//     public LinkedQueue pendingRequests;
-     private Queue<Loan> pendingRequests;
+public class  PendingRequestsQueue {
+
+     private static Queue<Loan> pendingRequests;
      public PendingRequestsQueue() {
          pendingRequests = new LinkedList<>();
      }
 
-     public void enqueue(String bookId , String memberId) {
-         Loan loan = new Loan(bookId, memberId);
+    static public void   enqueue(String bookId , String memberId) {
+         Loan loan = new Loan(bookId,memberId);
          pendingRequests.add(loan);
-         System.out.println("Your pending request has been enqueued: "+loan);
+         System.out.println("Your pending request has been enqueued: "+loan.getLoanID());
+
      }
 
      public Loan dequeue() {
@@ -24,14 +25,12 @@ public class PendingRequestsQueue {
          return loan;
      }
 
-    public void display() {
-        if (pendingRequests.isEmpty()) {
-            System.out.println("The queue is empty.");
-        } else {
-            System.out.println("Pending Requests in the queue:");
-            for (Loan request : pendingRequests){
-                System.out.println(request.toString());
-            }
+    public static void display() {
+ Loan temp =pendingRequests.peek();
+        while (temp != null) {
+            System.out.println(temp.getLoanID());
+            temp =temp.n
+
         }
     }
 }
