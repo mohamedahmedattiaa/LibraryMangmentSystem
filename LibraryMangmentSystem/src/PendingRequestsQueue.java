52ff337edd4ec24e1ee7bd73468c32aefc836fe7
mohamedahmedattiaa <mohamedahmedattia7@gmail.com>
2024-11-29 04:34:05 +0200

    update the methods

--- LibraryMangmentSystem/src/PendingRequestsQueue.java
+++ LibraryMangmentSystem/src/PendingRequestsQueue.java
@@ -1,36 +1,26 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class  PendingRequestsQueue {
+public class PendingRequestsQueue {
+    private static Queue<Loan> request = new LinkedList<>();
 
-     private static Queue<Loan> pendingRequests;
-     public PendingRequestsQueue() {
-         pendingRequests = new LinkedList<>();
-     }
+    public static void enqueue(Loan loan) {
+        request.add(loan);
+        System.out.println("Loan request added for book '" + loan.getBookId() + "' by member '" + loan.getMemberId() + "'.");
+    }
 
-    static public void   enqueue(String bookId , String memberId) {
-         Loan loan = new Loan(bookId,memberId);
-         pendingRequests.add(loan);
-         System.out.println("Your pending request has been enqueued: "+loan.getLoanID());
+    public static Loan dequeue() {
+        return request.poll();
+    }
 
-     }
-
-     public Loan dequeue() {
-         if (pendingRequests.isEmpty()) {
-             System.out.println("Your pending request is empty: " + pendingRequests.toString());
-             return null;
-         }
-         Loan loan = pendingRequests.poll();
-         System.out.println("Your pending request has been dequeued: " + loan.toString());
-         return loan;
-     }
+    public static boolean isEmpty() {
+        return request.isEmpty();
+    }
 
     public static void display() {
- Loan temp =pendingRequests.peek();
-        while (temp != null) {
-            System.out.println(temp.getLoanID());
-            temp =temp.n
-
+        System.out.println("Pending Loan Requests:");
+        for (Loan loan : request) {
+            System.out.println("Book ID: " + loan.getBookId() + ", Member: " + loan.getMemberId());
         }
     }
 }
