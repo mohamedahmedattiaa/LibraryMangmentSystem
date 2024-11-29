2595f440bfd4ad46681e4779660d8ebd509d21cb
mohamedahmedattiaa <mohamedahmedattia7@gmail.com>
2024-11-29 04:33:15 +0200

    some test cases

--- LibraryMangmentSystem/src/Main.java
+++ LibraryMangmentSystem/src/Main.java
@@ -3,24 +3,35 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        // Create Catalog to store books
         catalog catalog = new catalog();
+        PendingRequestsQueue pendingRequestsQueue = new PendingRequestsQueue();
 
-        // Create Books
+// Add books to the catalog
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "Fiction");
-        Book book2 = new Book("1984", "George Orwell", "Dystopian");
-        Book book3 = new Book("Moby Dick", "Herman Melville", "Adventure");
-
-        // Add books to the catalog
+        Book book2 = new Book("1984",  "George Orwell", "Dystopian");
+        Book book3 = new Book("The Zobr", "George Orwell", "Dystopian");
+        Book book4 = new Book("THE GREATEST ASS", "George Orwell", "Fiction");
         catalog.addBook(book1);
         catalog.addBook(book2);
         catalog.addBook(book3);
+        catalog.addBook(book4);
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
+// Borrow and return books
+        Loan loan = new Loan();
+        Member member1 = new Member("Ahmed", "Ahmed1121@gmail.com");
+        Member member2 = new Member("Mohamed", "Mohamed1122@gmail.com");
+        Member member3 = new Member("Ezz", "Ezz@gmail.com");
 
-        // Create Members
-        Member member1 = new Member("John Doe", "john.doe@example.com");
-        Member member2 = new Member("Alice Smith", "alice.smith@example.com");
+        loan.borrowBook(member1, "B100", catalog);
+        loan.borrowBook(member2, "B100", catalog);
+        loan.borrowBook(member1, "B101", catalog);
+        loan.borrowBook(member2, "B101", catalog);
+        loan.borrowBook(member3, "B102", catalog);
+        loan.borrowBook(member3, "B103", catalog);
+        loan.borrowBook(member1, "B103", catalog);
+        loan.returnBook(member1, "B100",catalog);
 
-        System.out.println(catalog.FindBookByTitle("MOBY DicK"));
+        PendingRequestsQueue.display();
 
 
     }
