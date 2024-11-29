758fa54220ded083f6de528bf2c8b997224f4e3d
AL-Shwadfy <doctorezz24@gmail.com>
2024-11-29 22:00:05 +0200

    Members LINKED lIST

---
+++ LibraryMangmentSystem/src/Members.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+
+public class Members {
+    private static LinkedList<Member> members = new LinkedList<>();
+    public static void add(Member member) {
+        members.add(member);
+    }
+    public static void remove(Member member) {
+        members.remove(member);
+    }
+    public static void display() {
+        System.out.println("Members:"+" "+members);
+    }
+    public static Member SearchMember(String memberID){
+String Temp = memberID;
+for(Member member: members) {
+    if (Temp.toLowerCase().equals(member.getmemberId().toLowerCase())) {
+        return member;
+    }
+    if (!Temp.toLowerCase().equals(member.getmemberId().toLowerCase())) {
+        continue;
+    }
+    else {
+        System.out.println("Member not found");
+        return null;
+    }
+}
+
+
+ return null;
+    }
+}
