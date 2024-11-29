4bac578bc047f62e8f0af28899356078a3f34b3a
AL-Shwadfy <doctorezz24@gmail.com>
2024-11-29 22:01:30 +0200

    Loan Edit

--- LibraryMangmentSystem/src/Loan.java
+++ LibraryMangmentSystem/src/Loan.java
@@ -47,22 +47,26 @@ public class Loan {
         this.returnDate = returnDate;
     }
 
-    public static void borrowBook(Member member, String bookId, catalog catalog) {
+    public static void borrowBook(String memberID, String bookId, catalog catalog) {
+        Member member = Members.SearchMember(memberID);
         Book book = catalog.searchBook(bookId);
         if (book != null && book.getAvailablityStatus()) {
             book.setAvailablityStatus(false);
             Loan loan = new Loan(bookId, member.getmemberId());
             System.out.println("Loan successfully created: " + loan.getLoanID() + " for book '" + book.getBookTitle() + "' by member '" + member.getName() + "'.");
-        } else {
+        } else if(book != null && !book.getAvailablityStatus()) {
             System.out.println("Book '" + bookId + "' is not available. Adding request for member '" + member.getName() + "'.");
             Loan loan = new Loan(bookId, member.getmemberId());
             PendingRequestsQueue.enqueue(loan);  // Add the loan request to the queue
         }
+        else {
+            System.out.println("Book with ID '" + bookId + "' not found in catalog.");
+        }
     }
 
-    public static void returnBook(Member member, String bookId, catalog catalog) {
+    public static void returnBook(String memberId, String bookId, catalog catalog) {
         Book book = catalog.searchBook(bookId);
-
+Member member = Members.SearchMember(memberId);
         if (book != null) {
             if (!book.getAvailablityStatus()) {
                 book.setAvailablityStatus(true);
@@ -75,9 +79,9 @@ public class Loan {
 
                 if (!PendingRequestsQueue.isEmpty()) {
                     Loan nextLoan = PendingRequestsQueue.dequeue();
-                    String nextMemberId = nextLoan.getMemberId();
-                    System.out.println("Processing next request for book: " + bookId + " for member: " + member.getName() + " , memberId: " + member.getmemberId());
-                    borrowBook(new Member(nextMemberId, "Unknown"), bookId, catalog);
+                    Member nextMember = Members.SearchMember(nextLoan.memberId);
+                    System.out.println("Processing next request for book: " + bookId + " for member: " + nextMember.getName() + " , memberId: " + nextMember.getmemberId());
+                    borrowBook(nextMember.getmemberId(), bookId, catalog); // edited
                 }
             } else {
                 System.out.println("Book '" + book.getBookTitle() + "' is already available.");
