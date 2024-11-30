e193ab62f981070d692ade95b3a56a74bbd4aeb4
Menna Tarek <mennatarek2048330@gmail.com>
2024-11-30 04:22:09 +0200

    add a login "GUI"

---
+++ LibraryMangmentSystem/src/LoginPage/Main.java
@@ -0,0 +1,12 @@
+package LoginPage;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[]args){
+        // create object from login page to see its working or not
+        new Login();
+        SwingUtilities.invokeLater(Login::new);
+
+    }
+}
