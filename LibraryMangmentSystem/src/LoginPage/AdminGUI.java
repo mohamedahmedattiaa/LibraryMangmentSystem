8f5586e6f53b01deb4451da61b5dcd2179993e7f
Menna Tarek <mennatarek2048330@gmail.com>
2024-12-06 01:42:10 +0200

    login

--- LibraryMangmentSystem/src/LoginPage/AdminGUI.java
+++ LibraryMangmentSystem/src/LoginPage/AdminGUI.java
@@ -1,7 +1,7 @@
 package LoginPage;
 import javax.swing.*;
 import java.awt.*;
-
+//vv
 public class AdminGUI extends JFrame {
     public AdminGUI() {
         JFrame frame = new JFrame("Admin panel");
